07894095888da1355c8c2e56fe434c189bbede58
sachin_savita <sachinsavita95@gmail.com>
2022-11-19 12:12:24 +0530

    BeanLifeCyple (Singleton, Prototype)

---
+++ spring_demo_one/src/com/spring/beans/BeanScopeDemoApp.java
@@ -0,0 +1,27 @@
+package com.spring.beans;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanScopeDemoApp {
+
+	public static void main(String[] args) {
+		
+		//load the spring configuration file
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanScope-applicationContext.xml");
+		//retrieve bean from spring container
+		Coach theCoach = context.getBean("myTrackCoach", Coach.class);
+		
+		Coach alphaCoach = context.getBean("myTrackCoach", Coach.class);
+		
+		boolean result = (theCoach==alphaCoach);
+		
+		System.out.println("Pointing to the same object: "+result);
+		
+		System.out.println("Memory location for theCoach: "+theCoach);
+		
+		System.out.println("Memory location for alphaCoach: "+alphaCoach);
+		
+
+	}
+
+}
