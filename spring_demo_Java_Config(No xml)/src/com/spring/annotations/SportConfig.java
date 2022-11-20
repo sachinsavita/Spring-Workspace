d4e41ae83f0f5c31528e2feaebb9a21aa6d3d5db
sachin_savita <sachinsavita95@gmail.com>
2022-11-20 23:04:15 +0530

    i

--- spring_demo_Java_Config(No xml)/src/com/spring/annotations/SportConfig.java
+++ spring_demo_Java_Config(No xml)/src/com/spring/annotations/SportConfig.java
@@ -3,6 +3,9 @@ package com.spring.annotations;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+
+//This class is added to scan components using @ComponentScan means whatever beans available it will track no xml file required
+
 @Configuration
 @ComponentScan("com.spring.annotations")
 //Now no need to mention  <context:component-scan base-package="com.spring.annotations"></context:component-scan> in applicationContext.xml file
